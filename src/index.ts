feb90934a117453f8df8cde5c2c6af618de63211
Kryštof Řezáč <krystofrezac@gmail.com>
2022-05-28 13:43:49 +0200

    Add eslint

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,15 @@
 import yargs from 'yargs/yargs';
 
+// eslint-disable-next-line no-unused-expressions
 yargs(process.argv.slice(2))
-.scriptName("doccheck")
-.usage('$0 <cmd> [args]')
-.command('check', 'Check if files are up to date', (yargs) => {
-}, (argv) => {
-  console.log("checking:", argv.file);
-}) 
-.option('f', {
-  alias: 'file',
-})
-.help()
-.argv;
+  .scriptName('doccheck')
+  .usage('$0 <cmd> [args]')
+  .command('check', 'Check if files are up to date', (yargs) => {
+  }, (argv) => {
+    console.log('checking:', argv.file);
+  })
+  .option('f', {
+    alias: 'file',
+  })
+  .help()
+  .argv;
