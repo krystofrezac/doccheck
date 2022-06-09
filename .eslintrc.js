71e72fc6658a58c08e5f27ac8f0c7b4884e0a412
Kryštof Řezáč <krystofrezac@gmail.com>
2022-06-09 19:11:24 +0200

    Add simple import sort

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,4 @@
+/* eslint-disable global-require */
 module.exports = {
   env: {
     es2021: true,
@@ -9,15 +10,35 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
-    project: "./tsconfig.json",
-    extraFileExtensions: ".js"
+    project: 'tsconfig.json',
+    extraFileExtensions: '.js',
   },
-  plugins: ['@typescript-eslint', 'prettier'],
+  plugins: ['@typescript-eslint', 'prettier', 'simple-import-sort'],
   rules: {
     'prettier/prettier': 'error',
     '@typescript-eslint/explicit-function-return-type': [
       'error',
       { allowExpressions: true },
+    ],
+    'simple-import-sort/exports': 'error',
+    'simple-import-sort/imports': [
+      'error',
+      {
+        groups: [
+          // Node.js builtins
+          [`^(${require('module').builtinModules.join('|')})(/|$)`],
+          // Packages. `react` related packages come first.
+          ['^@?\\w'],
+          // Absolute imports
+          ['^(commands|utils)(/.*|$)'],
+          // Side effect imports.
+          ['^\\u0000'],
+          // Parent imports. Put `..` last.
+          ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+          // Other relative imports. Put same-folder imports and `.` last.
+          ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+        ],
+      },
     ],
   },
 };
