23d1a5eceaaff2b7791d3ce6284e7f20d9ab7fdc
Kryštof Řezáč <krystof.rezac@alza.cz>
2022-06-12 09:20:49 +0200

    Add docuaurus

---
+++ docs/docs/intro.md
@@ -0,0 +1,8 @@
+---
+slug: /
+sidebar_position: 1
+updatedAfter: 5c114e15b66b7679ad2567b5a6bc4b891b2333cc
+dep: src/index.ts
+---
+
+# Getting started
---
+++ docs/docusaurus.config.js
@@ -0,0 +1,100 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Doccheck',
+  url: 'https://your-docusaurus-test-site.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  // favicon: 'img/favicon.ico',
+
+  organizationName: 'krystofrezac', // Usually your GitHub org/user name.
+  projectName: 'doccheck', // Usually your repo name.
+
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          routeBasePath: '/',
+          sidebarPath: require.resolve('./sidebars.js'),
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        blog: false,
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'Doccheck documentation',
+        /* logo: {
+          alt: 'Doccheck log',
+          src: 'img/logo.svg',
+        }, */
+        items: [
+          {
+            href: 'https://github.com/krystofrezac/doccheck',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      /*
+      footer: {
+        style: 'dark',
+        links: [
+           {
+            title: 'Community',
+            items: [
+              {
+                label: 'Stack Overflow',
+                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+              },
+              {
+                label: 'Discord',
+                href: 'https://discordapp.com/invite/docusaurus',
+              },
+              {
+                label: 'Twitter',
+                href: 'https://twitter.com/docusaurus',
+              },
+            ],
+          }, 
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'GitHub',
+                href: 'https://github.com/krystofrezac/doccheck',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      }, */
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
